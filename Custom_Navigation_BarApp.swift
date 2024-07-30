039d24005aaabf82a7ffb265f964c33a5f53d10b
Priyahansaliya <112390812+Priyahansaliya@users.noreply.github.com>
2024-07-30 11:23:00 +0530

    Add files via upload

---
+++ Custom_Navigation_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Navigation_BarApp.swift
+//  Custom Navigation Bar
+//
+//  Created by Priya Hansaliya on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Navigation_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
